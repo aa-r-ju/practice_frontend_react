4b8730a4402fcdf1d3053999ba85d24473781db2
aa-r-ju <aarju3447@gmail.com>
2023-08-09 15:53:52 +0545

    Feat:remove all the hard code from the index.js file

--- src/index.js
+++ src/index.js
@@ -7,26 +7,6 @@ import axios from 'axios'
 
 axios.get('http://localhost:3001/notes').then(response => {
   const notes = response.data
-  console.log(notes)
+  ReactDOM.createRoot(document.getElementById('root')).render(<App notes={notes} />)
 })
-const notes = [
-  {
-    id: 1,
-    content: 'HTML is easy',
-    important: true
-  },
-  {
-    id: 2,
-    content: 'Browser can execute only JavaScript',
-    important: false
-  },
-  {
-    id: 3,
-    content: 'GET and POST are the most important methods of HTTP protocol',
-    important: true
-  }
-]
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <App notes={notes} />
-)
