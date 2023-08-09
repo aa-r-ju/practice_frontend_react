f91d589aa3477aa17664a324aec54e8432df7e8a
aa-r-ju <aarju3447@gmail.com>
2023-08-09 15:36:24 +0545

    Feat:used .then method to return the promise

--- src/index.js
+++ src/index.js
@@ -6,11 +6,10 @@ import App from './App'
 import axios from 'axios'
 
 const promise = axios.get('http://localhost:3001/notes')
-console.log(promise)
 
-// const promise2 = axios.get('http://localhost:3001/foobar')
-// console.log(promise2)
-
+promise.then(response => {
+  console.log(response)
+})
 const notes = [
   {
     id: 1,
