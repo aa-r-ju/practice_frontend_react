effee7e5718eeefa5fa97a63bd50be6a346e51d7
aa-r-ju <aarju3447@gmail.com>
2023-08-09 15:38:48 +0545

    Feat:remove variable name from axios

--- src/index.js
+++ src/index.js
@@ -5,10 +5,9 @@ import App from './App'
 
 import axios from 'axios'
 
-const promise = axios.get('http://localhost:3001/notes')
-
-promise.then(response => {
-  console.log(response)
+axios.get('http://localhost:3001/notes').then(response => {
+  const notes = response.data
+  console.log(notes)
 })
 const notes = [
   {
